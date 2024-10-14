ac51a83bc3e1c12a8205b7f2805ab2aa2fb4d0ab
getokeys <arushigoel50@gmail.com>
2024-10-14 09:49:09 +0530

    5th commit

---
+++ day5/script.js
@@ -0,0 +1,26 @@
+//function display(){
+//  alert("hello")
+//}
+//let x=12;
+//function display(){
+  //  console.log("hello js",x);
+//}
+//let x=14;
+//console.log("hello js",x);
+//display();
+//let ar=[1,2,3,4,5];
+//let a;
+//let b= null;
+//console=log(typeof b);
+function validate(){
+    let un=document.getElementById('un').value;
+    let ps=document.getElementById('ps').value;
+    if(un==='ar'&& ps==='123'){
+        alert("login successful");
+    }
+    else{
+        alert("wrong");
+    }
+
+}
+
